b8352c434dc9a110894fd8f5c3fee13214353eef
Cui-y <cuiyang673308817@gmail.com>
2019-04-25 13:04:54 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { watch, src, dest, series } = require('gulp')
+const htmlminplugin = require('gulp-htmlmin')
+const browserSync = require('browser-sync').create()
+
+function copy () {
+  return src('src/**').pipe(dest('dist'))
+}
+
+function dev () {
+  watch('./src/*.html').on('change', browserSync.reload)
+
+  return browserSync.init({
+    server: {
+      baseDir: './src'
+    }
+  })
+}
+
+function htmlmin () {
+  return src('src/*.html')
+    .pipe(htmlminplugin({ collapseWhitespace: true }))
+    .pipe(dest('dist'))
+}
+
+let build = series(copy, htmlmin)
+
+exports.build = build
+exports.default = dev
